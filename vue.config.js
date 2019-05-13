dba4577709a8635950014310b4a9d116b48900de
Tagore <tagore@dronemapp.com>
2019-05-13 07:05:10 -0300

    first commit

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  pwa: {
+    name: 'Vue Meetup PWA'
+  },
+
+  pluginOptions: {
+    i18n: {
+      locale: 'pt-BR',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: false
+    }
+  }
+}
